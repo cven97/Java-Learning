d52fa643119897727e343c2ecbf800458c75ff1c
lompo <lompolrnt@email.com>
2024-04-29 13:08:56 +0000

    création de la methode de calcul de superficie d'un rectangle

--- src/main/java/bf/sonabel/javalearning/JavaLearning.java
+++ src/main/java/bf/sonabel/javalearning/JavaLearning.java
@@ -6,11 +6,27 @@ package bf.sonabel.javalearning;
 
 /**
  *
- * @author ouare
+ * @author lompo
  */
 public class JavaLearning {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    
+    /*
+    
+    ** Founction de calcul de superficie d'un rectangle 
+    
+     *@param longueur 
+     *@param largeur
+     *@return float
+    
+    */
+    
+    public float calculSuperficieRectangle(final float longueur, final float largeur){
+        return longueur*largeur;
+    }
+    
 }
